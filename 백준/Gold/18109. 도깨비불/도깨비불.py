e5fdb81462c81686c8ae882265ace21294f162a0
kwondoha <ehehgk@naver.com>
2024-05-31 15:16:38 +0900

    [Gold IV] Title: 도깨비불, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/18109. 도깨비불/도깨비불.py
+++ 백준/Gold/18109. 도깨비불/도깨비불.py
@@ -2,6 +2,10 @@
 arr=input()
 arr=list(arr)
 
+first=['r','R','s','e','E','f','a','q','Q','t','T','d','w','W','c','z','x','v','g']
+second=['k','o','i','O','j','p','u','P','h','hk','ho','hl','y','n','nj','np','nl','b','m','ml','l']
+third=['r', 'R', 'rt', 's', 'sw', 'sg', 'e', 'f', 'fr', 'fa', 'fq', 'ft', 'fx', 'fv', 'fg', 'a', 'q', 'qt', 't', 'T', 'd', 'w', 'c', 'z', 'x', 'v', 'g']
+
 i=0
 while i <len(arr)-1: #중성 정리
   if arr[i] == 'h' and arr[i+1] in ['k','o','l']:
@@ -18,15 +22,6 @@ while i <len(arr)-1: #중성 정리
     i+=2
   else:
     i+=1
-
-
-first=['r','R','s','e','E','f','a','q','Q','t','T','d','w','W','c','z','x','v','g']
-second=['k','o','i','O','j','p','u','P','h','hk','ho','hl','y','n','nj','np','nl','b','m','ml','l']
-third=['r', 'R', 'rt', 's', 'sw', 'sg', 'e', 'f', 'fr', 'fa', 'fq', 'ft', 'fx', 'fv', 'fg', 'a', 'q', 'qt', 't', 'T', 'd', 'w', 'c', 'z', 'x', 'v', 'g']
-
-cnt_sec = sum(1 for i in arr if i in second) #모음의 개수
-
-do=0
 
 i=0
 while i <len(arr)-2: #종성 정리
@@ -49,14 +44,7 @@ while i <len(arr)-2: #종성 정리
   else:
     i+=1
 
-#마지막 받침이 쌍받침 또는 겹받침인 경우
-if (arr[-1] == 't' and arr[-2] in ['r','q']) or (arr[-1] in ['w','g'] and arr[-2] == 's') :
-  arr[-2]=arr[-2]+arr[-1]
-  del(arr[-1])
-elif (arr[-1] in ['r','a','q','t','x','v','g'] and arr[-2] == 'f'):
-  arr[-2]=arr[-2]+arr[-1]
-  del(arr[-1])
-
+do=0 #도깨비불
 for i in range(len(arr)-2):
   if arr[i-1] in first and arr[i] in second and arr[i+1] in third and arr[i+2] in second:
     do+=1
